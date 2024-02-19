aa24864b978923d12b6b79afff598b6e9a5f2ca0
unknown <chicolemolina@gmail.com>
2024-02-19 15:24:17 +0100

    error action.tx 3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,7 +74,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+  const { customerId, amount, status } = updateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
